7b15d14a8cc5a538d401cc1c8527152b22f748ba
Kari Evergreen <evergreenkary@gmail.com>
2023-02-23 18:38:09 +0800

    chore: remove unused files

--- zkdoc_sdk/src/circuits/mod.rs
+++ zkdoc_sdk/src/circuits/mod.rs
@@ -1,7 +1,2 @@
-pub mod file_hash_partial;
-pub mod poseidon;
-pub mod poseidon_v2;
-pub mod file_hash_partial_v2;
-pub mod file_hash_partial_v3;
-pub mod file_hash_partial_v4;
 pub mod file_hasher;
+pub mod poseidon;
--- zkdoc_sdk/src/gadgets/mod.rs
+++ zkdoc_sdk/src/gadgets/mod.rs
@@ -1,5 +1,4 @@
-pub mod poseidon;
+pub mod assert_is_equal;
 pub mod file_hash_row_selector;
 pub mod file_selector_accumulator;
-pub mod assert_is_equal;
-pub mod poseidon_v2;
+pub mod poseidon;
